4df09f76efd44a982e87403946c6cd574089e6f7
greyspectrum <hello@greyspectrum.io>
2022-04-04 08:30:57 -0400

    read from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 #[derive(Debug)]
 struct Arguments {
     target: String,
@@ -37,5 +39,22 @@ fn parse_args() -> Arguments {
 
 fn main() {
     let args = parse_args();
-    println!("{:?}", args);
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to read from file '{}' : {:?}",
+                      "Error:".red().bold(), args.filename, e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, &data) {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("{} failed to write to file '{}': {:?}",
+                      "Error:".red().bold(), args.filename, e);
+            std::process::exit(1);
+        }
+    };
 }
